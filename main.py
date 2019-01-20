36f3885c7fc48d74a570d76b39a1da783ee9d877
Faheem Khan <faheem5948@gmail.com>
2019-01-19 23:36:41 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -48,4 +48,4 @@ def return_image():
     return send_file("image.png", attachment_filename="image.png")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host= '0.0.0.0', port=5000, debug=False)
